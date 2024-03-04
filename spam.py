2239f34773171c81dd5507d3d47656d5deeda7dc
hlafaille <hlafaille@proton.me>
2024-03-04 15:40:57 +0000

    #10 adding graalvm support

--- spam.py
+++ spam.py
@@ -2,6 +2,6 @@ import subprocess
 import datetime
 
 if __name__ == "__main__":
-    for x in range(0, 1):
+    for x in range(0, 100):
         process = subprocess.Popen(["curl", "-k", "-i", "-H", "'Host: api.weather.gov'", "https://localhost:8443"], stderr=subprocess.DEVNULL)
         
