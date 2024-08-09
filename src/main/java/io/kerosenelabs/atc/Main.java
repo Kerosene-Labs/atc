a31d981104aac7a3fc264ecbc190e3013d028388
hlafaille <hlafaille@proton.me>
2024-08-09 15:19:41 +0000

    #30 re-implementing logback

--- src/main/java/io/kerosenelabs/atc/Main.java
+++ src/main/java/io/kerosenelabs/atc/Main.java
@@ -11,21 +11,17 @@ import io.kerosenelabs.kindling.handler.RequestHandler;
 
 public class Main {
     public static void main(String[] args) throws KindlingException {
-        // 
         KindlingServer server = KindlingServer.getInstance();
-
 
         server.installRequestHandler(new RequestHandler() {
 
             @Override
             public boolean accepts(HttpMethod arg0, String arg1) throws KindlingException {
-                // TODO Auto-generated method stub
                 throw new UnsupportedOperationException("Unimplemented method 'accepts'");
             }
 
             @Override
             public HttpResponse handle(HttpRequest arg0) throws KindlingException {
-                // TODO Auto-generated method stub
                 throw new UnsupportedOperationException("Unimplemented method 'handle'");
             }
 
